c9cbce2b9e44a510c06c3e6415a3462e573cdb49
sik2 <bickwang123@gmail.com>
2023-10-27 12:37:49 +0900

    memberRepository

--- src/main/java/org/example/member/repository/MemberRepository.java
+++ src/main/java/org/example/member/repository/MemberRepository.java
@@ -1,4 +1,36 @@
 package org.example.member.repository;
 
+import org.example.member.entity.Member;
+import util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+    List<Member> memberList = new ArrayList<>();
+
+    public MemberRepository () {
+        Member member1 = new Member(1, "user1", "1234",  Util.nowDateTime());
+        memberList.add(member1);
+        Member member2 = new Member(2, "user2", "1234",  Util.nowDateTime());
+        memberList.add(member2);
+        Member member3 = new Member(3, "user3", "1234",  Util.nowDateTime());
+        memberList.add(member3);
+    }
+
+
+    public Member getMemberFindByUserId(String userId) {
+        for (int i = 0; i < memberList.size(); i++) {
+            Member member = memberList.get(i);
+            if (member.getUserId().equals(userId)) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public void join(String userId, String password) {
+        Member member = new Member(userId, password, Util.nowDateTime());
+        memberList.add(member);
+    }
 }
--- src/main/java/org/example/member/service/MemberService.java
+++ src/main/java/org/example/member/service/MemberService.java
@@ -1,37 +1,16 @@
 package org.example.member.service;
 
 import org.example.member.entity.Member;
-import util.Util;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import org.example.member.repository.MemberRepository;
 
 public class MemberService {
-    List<Member> memberList = new ArrayList<>();
-
-    public MemberService () {
-        Member member1 = new Member(1, "user1", "1234",  Util.nowDateTime());
-        memberList.add(member1);
-        Member member2 = new Member(2, "user2", "1234",  Util.nowDateTime());
-        memberList.add(member2);
-        Member member3 = new Member(3, "user3", "1234",  Util.nowDateTime());
-        memberList.add(member3);
-    }
-
+    MemberRepository memberRepository = new MemberRepository();
 
     public Member getMemberFindByUserId(String userId) {
-        for (int i = 0; i < memberList.size(); i++) {
-            Member member = memberList.get(i);
-            if (member.getUserId().equals(userId)) {
-                return member;
-            }
-        }
-        return null;
+        return this.memberRepository.getMemberFindByUserId(userId);
     }
 
     public void join(String userId, String password) {
-        Member member = new Member(userId, password, Util.nowDateTime());
-        memberList.add(member);
+        this.memberRepository.join(userId, password);
     }
 }
